c52d4023103f1bfe0c0c67004d2470f649782215
Yami <yami@metanext.biz>
2023-08-11 15:27:32 +0330

    fix is url

--- lib/ext/string.dart
+++ lib/ext/string.dart
@@ -56,10 +56,12 @@ extension StringNullExtensions on String? {
     if (isEmptyOrNull) return false;
     return !this!.trim().isEmptyOrNull;
   }
+  bool get isURl => RegExp(
+      r'^((?:.|\n)*?)((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?)')
+      .hasMatch((!isEmptyOrNull) ? this! :''  );
 }
-bool isURl(String url){
-  return  RegExp(r'^((?:.|\n)*?)((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?)')
-      .hasMatch(url);  }
+
+
 
 const _htmlColorNames = {
   'aliceblue': '#f0f8ff',
